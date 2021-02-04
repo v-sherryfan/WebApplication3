4267920f2709115f16fa8ad305f639bd1b1ebb2a
Sara Shi (CSI Interfusion Inc) <v-qushi@microsoft.com>
2021-02-03 23:02:09 -0800

    Projektdateien hinzufügen.

---
+++ WebApplication3/Startup.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication3
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseMvc();
+            app.Run(async (context) =>
+            {
+                if (context.Request.Path.Value.Contains("checkprime"))
+                {
+                    int numberToCheck;
+                    try
+                    {
+                        numberToCheck = int.Parse(context.Request.QueryString.Value.Replace("?", ""));
+                        var primeService = new PrimeService();
+                        if (primeService.IsPrime(numberToCheck))
+                        {
+                            await context.Response.WriteAsync(numberToCheck + " is prime!");
+                        }
+                        else
+                        {
+                            await context.Response.WriteAsync(numberToCheck + " is NOT prime!");
+                        }
+                    }
+                    catch
+                    {
+                        await context.Response.WriteAsync("Pass in a number to check in the form /checkprime?5");
+                    }
+                }
+                else
+                {
+                    await context.Response.WriteAsync("Hello World! To check if a number is prime, provide URL of the form /checkprime?5");
+                }
+            });
+
+        }
+    }
+}
